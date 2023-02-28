55f0eb53b3fcd6282c4345713cd46915686693e6
dan <djosephhenri@gmail.com>
2023-02-28 17:40:57 +0900

    added r_ele

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::fs;
 struct Entry {
     ent_seq: Vec<i32>,
     k_ele: Vec<KEle>,
+    r_ele: Vec<REle>,
+    sense: Vec<Sense>,
 }
 
 #[derive(Debug)]
@@ -12,6 +14,31 @@ struct KEle {
     keb: String,
     ke_inf: String,
     ke_pri: String,
+}
+
+#[derive(Debug)]
+struct REle {
+    reb: String,
+    re_nokanji: String,
+    re_restr: String,
+    re_inf: String,
+    re_pri: String,
+}
+
+#[derive(Debug)]
+struct Sense {
+    stagk: String,
+    stagr: String,
+    xref: String,
+    ant: String,
+    pos: String,
+    field: String,
+    misc: String,
+    lsource: String,
+    dial: String,
+    gloss: Vec<String>,
+    pri: String,
+    s_inf: String,
 }
 
 fn main() {
@@ -27,9 +54,7 @@ fn main() {
 
     let doc = Document::parse_with_options(&dict_string, opt).unwrap();
 
-    // let mut ent_seq_vec: Vec<i32> = vec![];
     let mut entries: Vec<Entry> = vec![];
-
     // Get all entries
     for node in doc.descendants() {
         // For each entry
@@ -39,8 +64,9 @@ fn main() {
 
             // Create vector for k_ele
             let mut k_ele_vec: Vec<KEle> = vec![];
+            let mut r_ele_vec: Vec<REle> = vec![];
 
-            for node in node.descendants() {
+            for node in node.children() {
                 if node.has_tag_name("ent_seq") {
                     ent_seq_vec.push(
                         node.text()
@@ -50,35 +76,101 @@ fn main() {
                     );
                 }
                 if node.has_tag_name("k_ele") {
-                    for node in node.descendants() {
-                        let mut keb = String::new();
-                        let mut ke_inf = String::new();
-                        let mut ke_pri = String::new();
+                    let mut keb = String::new();
+                    let mut ke_inf = String::new();
+                    let mut ke_pri = String::new();
+
+                    for node in node.children() {
                         if node.has_tag_name("keb") {
                             keb = node.text().expect("error finding keb text").to_string();
-                        }
-                        if node.has_tag_name("ke_inf") {
+                        } else if node.has_tag_name("ke_inf") {
                             ke_inf = node.text().expect("error finding ke_inf text").to_string();
-                        }
-                        if node.has_tag_name("ke_pri") {
+                        } else if node.has_tag_name("ke_pri") {
                             ke_pri = node.text().expect("error finding ke_pri text").to_string();
                         }
-                        println!("keb {}", &keb);
-                        let k_ele = KEle {
-                            keb,
-                            ke_inf,
-                            ke_pri,
-                        };
-                        // k_ele_vec.push(k_ele);
+                    }
+                    let k_ele = KEle {
+                        keb,
+                        ke_inf,
+                        ke_pri,
+                    };
+                    k_ele_vec.push(k_ele);
+                }
+                if node.has_tag_name("r_ele") {
+                    let mut reb = String::new();
+                    let mut re_nokanji = String::new();
+                    let mut re_restr = String::new();
+                    let mut re_inf = String::new();
+                    let mut re_pri = String::new();
+
+                    for node in node.children() {
+                        if node.has_tag_name("reb") {
+                            reb = node.text().expect("error finding reb text").to_string();
+                        } else if node.has_tag_name("re_nokanji") {
+                            if let Some(t) = node.text() {
+                                re_nokanji = t.to_string();
+                            }
+                        } else if node.has_tag_name("re_restr") {
+                            re_restr = node
+                                .text()
+                                .expect("error finding re_restr text")
+                                .to_string();
+                        } else if node.has_tag_name("re_inf") {
+                            re_inf = node.text().expect("error finding re_inf text").to_string();
+                        } else if node.has_tag_name("re_pri") {
+                            re_pri = node.text().expect("error finding re_pri text").to_string();
+                        }
+                    }
+                    let r_ele = REle {
+                        reb,
+                        re_nokanji,
+                        re_restr,
+                        re_inf,
+                        re_pri,
+                    };
+                    r_ele_vec.push(r_ele);
+                }
+                if node.has_tag_name("sense") {
+                    let mut stagk = String::new();
+                    let mut stagr = String::new();
+                    let mut xref = String::new();
+                    let mut ant = String::new();
+                    let mut pos = String::new();
+                    let mut field = String::new();
+                    let mut misc = String::new();
+                    let mut lsource = String::new();
+                    let mut dial = String::new();
+                    let mut gloss: Vec<String> = vec![];
+                    let mut pri = String::new();
+                    let mut s_inf = String::new();
+
+                    for node in node.children() {
+                        if node.has_tag_name("stagk") {
+                            stagk = node.text().expect("error finding stagk text").to_string();
+                        } else if node.has_tag_name("stagr") {
+                            stagr = node.text().expect("error finding stagr text").to_string();
+                        } else if node.has_tag_name("xref") {
+                            xref = node.text().expect("error finding xref text").to_string();
+                        } else if node.has_tag_name("ant") {
+                            ant = node.text().expect("error finding ant text").to_string();
+                        } else if node.has_tag_name("pos") {
+                            pos = node.text().expect("error finding pos text").to_string();
+                        } else if node.has_tag_name("field") {
+                            field = node.text().expect("error finding field text").to_string();
+                        } else if node.has_tag_name("misc") {
+                            misc = node.text().expect("error finding misc text").to_string();
+                        }
                     }
                 }
             }
+
             let entry = Entry {
                 ent_seq: ent_seq_vec,
                 k_ele: k_ele_vec,
+                r_ele: r_ele_vec,
             };
             entries.push(entry);
         }
     }
-    // println!("{entries:?}");
+    println!("{entries:?}");
 }
